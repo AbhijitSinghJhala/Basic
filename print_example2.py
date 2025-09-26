9d951944c1e06b4380ac3c68bc394fb491bb3db2
Abhijit <abhijitsinghjhala11@gmail.com>
2025-09-26 11:01:49 +0530

    updated

---
+++ print_example2.py
@@ -0,0 +1,24 @@
+name = "Tops \"Technologies\""
+print(name)
+
+name1 = 'Tops \'Technologies\''
+print(name1)
+
+name2 = "Tops \n Technologies"
+print(name2)
+
+name3 = "Tops \t Technology"
+print(name3)
+
+name4 = "Tops \bTechnology"
+print(name4)
+
+print("tops","Technologies",sep="\n")
+
+print("tops","technology",sep="\t")    
+
+print("tops technology", end="\n")
+print("tops","technology",end="\n")
+
+print("tops technology",end="\t")
+print("tops", "technology",end="\t")
